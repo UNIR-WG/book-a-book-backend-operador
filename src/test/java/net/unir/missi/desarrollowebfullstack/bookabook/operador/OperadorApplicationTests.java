aa6396b206f75ea1f4604842a4da00d35580359a
AleixMT <aleixaretra@gmail.com>
2024-01-16 22:16:54 +0100

    ADDED: operador commit inicial

---
+++ src/test/java/net/unir/missi/desarrollowebfullstack/bookabook/operador/OperadorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.unir.missi.desarrollowebfullstack.bookabook.operador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
