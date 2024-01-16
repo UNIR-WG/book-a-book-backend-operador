9830e3e1bf021ee29ef07934d61847962a7d9706
AleixMT <aleixaretra@gmail.com>
2024-01-16 23:33:44 +0100

    ADDED: communication with eureka

---
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/operador/controller/EurekaGreetingController.java
@@ -0,0 +1,8 @@
+package net.unir.missi.desarrollowebfullstack.bookabook.operador.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface EurekaGreetingController {
+
+    String greeting();
+}
---
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/operador/controller/EurekaGreetingControllerImpl.java
@@ -0,0 +1,26 @@
+package net.unir.missi.desarrollowebfullstack.bookabook.operador.controller;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaGreetingControllerImpl implements EurekaGreetingController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Override
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return String.format(
+                "Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
+}
