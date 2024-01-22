442493c22c12f12dc2ba3f41fec321a422e482e2
AleixMT <aleixaretra@gmail.com>
2024-01-22 14:16:53 +0100

    CHANGED: db name and added operation in main api of operator

--- src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/operador/config/BuscadorClient.java
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/operador/config/BuscadorClient.java
@@ -1,10 +1,17 @@
 package net.unir.missi.desarrollowebfullstack.bookabook.operador.config;
 
+import net.unir.missi.desarrollowebfullstack.bookabook.operador.data.model.api.AuthorRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(name = "buscador")
 public interface BuscadorClient {
     @GetMapping(value = "/hello")
     String hello();
+
+    @GetMapping("/authors")
+    ResponseEntity<List<AuthorRequest>> getAuthors();
 }
