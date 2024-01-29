7aea4af1ffde294174699127cd970cde200f8bdf
AleixMT <aleixaretra@gmail.com>
2024-01-29 22:47:36 +0100

    CHANGED: url for feign client

--- src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/operador/clients/BuscadorClient.java
+++ src/main/java/net/unir/missi/desarrollowebfullstack/bookabook/operador/clients/BuscadorClient.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "buscador", url = "http://localhost:8082/",configuration = FeignConfig.class)
+@FeignClient(name = "buscador", configuration = FeignConfig.class)
 public interface BuscadorClient {
 
 
